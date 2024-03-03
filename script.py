07cccc536e9a4e792d0692db3f249aac191e534a
vovan <vovan@nimble.com>
2024-03-03 14:51:45 +0100

    fix

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import datetime
 import platform 
 
-artifact_file = "artifact.txt"
+artifact_file = "/tmp/artifact.txt"
 
 with open(artifact_file, "w") as f:
     f.write("DATE/TIME: " + str(datetime.datetime.now())+"\n")
